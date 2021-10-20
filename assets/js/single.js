10ef75dd204177c5a706d4b7cb28ee5901abd71e
davidjaguilar104 <davidjaguilar104@gmail.com>
2021-10-20 12:18:57 -0500

    page redirection and error handling

--- assets/js/single.js
+++ assets/js/single.js
@@ -2,6 +2,25 @@ var issueContainerEl = document.querySelector("#issues-container");
 
 var limitWarningEl = document.querySelector("#limit-warning");
 
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function() {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    
+    if(repoName) {
+        // display repo name on the page
+        repoNameEl.textContent = repoName;
+
+        // pass repo name to getRepoIssues function
+        getRepoIssues(repoName);
+    } else {
+        // if no repo was given, redirect to the homepage
+        document.location.replace("./index.html")
+    }
+};
+
 var displayWarning = function(repo) {
     // add text to warning container
     limitWarningEl.textContent = "To see more than 30 issues, visit ";
@@ -33,12 +52,11 @@ var getRepoIssues = function(repo) {
             });
         } 
         else {
-            alert("There was a problem with your request!");
+            // if not successful, redirect to homepage
+            document.location.replace("./index.html");
         } 
     });
 };
-
-getRepoIssues("facebook/react");
 
 var displayIssues = function(issues) {
     if(issues.length === 0) {
@@ -78,3 +96,5 @@ var displayIssues = function(issues) {
         issueContainerEl.appendChild(issueEl);
     }
 };
+
+getRepoName();
